fe3779583a335a6e9c9052b80e2bab25136d3033
Jeff Reding <den_jeff@hotmail.com>
2018-04-11 17:26:00 +0200

    javascript kurs erster teil start

---
+++ javascript/main.js
@@ -0,0 +1,29 @@
+//alert("Hello world");
+
+let nachricht = "Hallo Welt";
+//alert(nachricht)
+nachricht = "neuer text";
+//alert(nachricht);
+console.log(nachricht);
+const linkColor = "#ff0000";
+let eine_variable = "test";
+console.log("variable: eine_variable: ", eine_variable);
+let highscore = 73847;
+console.log("Highscore ist: ", highscore / 20);
+let meiennachricht = "test1"+
+    "und so weiter"
+
+meinenachricht= "test1";
+console.log(meinenachricht);
+meinenachricht = `Ein 
+und ein 
+ganz langer 
+text 
+und aus`;
+console.log(meinenachricht);
+let fullname = `Jeffrey"the dude"Lebowky`
+
+let isOver18=true;
+console.log("über 18?", isOver18)
+let age = 17;
+console.log("age über 18", age > 18)
